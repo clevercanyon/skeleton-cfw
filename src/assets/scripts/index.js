5e7225ab89dc55469d9ea62412196e273200279f
Mike Taykle <miketaykle@o5p.me>
2022-04-24 07:11:12 -0400

    update

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -3,7 +3,17 @@
  *
  * @since 2022-02-26
  */
+// <editor-fold desc="Imports and other headers.">
 /* eslint-env worker, es6 */
+
+/**
+ * Imports.
+ *
+ * @since 2022-02-26
+ */
+// Nothing at this time.
+
+// </editor-fold>
 
 /**
  * Adds event listener.
